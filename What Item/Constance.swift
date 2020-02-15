002e8c9feca7476c47ea33961a5f8be42634e16c
mishadovhiy <mishadovhiy@gmail.com>
2020-02-15 18:58:35 +0200

    image recognition - done

---
+++ What Item/Constance.swift
@@ -0,0 +1,25 @@
+//
+//  Constance.swift
+//  What Item
+//
+//  Created by Misha Dovhiy on 12.02.2020.
+//  Copyright © 2020 Misha Dovhiy. All rights reserved.
+//
+
+import UIKit
+
+struct K {
+    static let resultsSegue = "goToResults"
+    static let imgaveViewHeight = "ImgaveViewHeight"
+}
+
+struct V {
+    static var userWord = ""
+    static var itemOnImage = ""
+    static var ifCorrect:Bool = false
+    static var textHolder = ""
+}
+
+struct Colors {
+    static var background = UIColor(named: "backgroundColor")
+}
---
+++ What Item/Controllers/ResultsVC.swift
@@ -0,0 +1,51 @@
+//
+//  ResultsVC.swift
+//  What Item
+//
+//  Created by Misha Dovhiy on 12.02.2020.
+//  Copyright © 2020 Misha Dovhiy. All rights reserved.
+//
+
+import UIKit
+
+class ResultsVC: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+        navigationBarStyle()
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+          return .lightContent
+    }
+    
+    func updateUI() {
+        
+        if V.ifCorrect == true {
+            titleLabel.text = "Well Done!"
+            textLabel.text = "You've found a \(V.userWord)"
+            titleLabel.textAlignment = .left
+            textLabel.textAlignment = .left
+        } else if V.ifCorrect == false {
+            titleLabel.text = "Oh no!"
+            textLabel.text = "Seems like it's not a \(V.userWord)\nIt looks like \(V.itemOnImage)"
+            titleLabel.textAlignment = .right
+            textLabel.textAlignment = .right
+        }
+    }
+    
+    func navigationBarStyle() {
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.barTintColor = Colors.background
+        navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+
+    }
+
+}
